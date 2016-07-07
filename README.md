b4605dff2742c13d71b01b3dc6a45bef76f37a0e
pitrack <xiapatrick418@hotmail.com>
2016-07-07 19:27:56 -0400

    Improved perf significantly, added single-player mode, fixed small regen bug

--- README.md
+++ README.md
@@ -6,27 +6,56 @@ Assist tool/solver for Codenames, a board game by Vlaada Chvatil
 
 Each word is embedded into a vector space using [GloVe](http://nlp.stanford.edu/projects/glove/). For each word (from the GloVe wiki corpus), we score it against the 25 code names in terms of similarity. We ultimately select the best word and the number of code names that it is associated with.
 
-This is a still a crude tool and word associations aren't perfect. As a result, `pickBest` does not always pick the best word. However, there is usually an acceptable clue in the output of `findWords()`.
+This is a still a crude tool and word associations aren't perfect. As a result, `pickBest()` (and `move()` does not always pick the best word. However, there is usually an acceptable clue in the output of `findWords()`.
 
 ## Usage
+
+In your shell, run the python interpreter (replace GLOVE_VECTORS.txt with a vector file).
 
 ```
 python -i sim.py GLOVE_VECTORS.txt word-list.txt
 ```
 
+### Single-player mode
+
+To view the board:
+
+```
+>>> board()
+```
+
+To generate the next clue:
+
+```
+>>> move()
+('accelerates', 3)
+```
+
+To guess something:
+```
+>>> guess('time')
+Correct!
+```
+
+Rules are currently up to the user to enforce. There is no detection for when the game ends.
+
+### Detailed Usage
+
 After loading the vectors, in the interpreter run:
 
 ```
 >>> clues = findWords()
->>> pickBest(clues)
+>>> pickBest(clues)   //This is the same output of moves()
 ('ants', 6)
 >>> words
 ['vet', 'pants', 'cotton', 'platypus', 'grass', 'horse', 'bugle', 'roulette', 'change', 'himalayas', 'fish', 'ninja', 'satellite', 'chest', 'dwarf', 'fan', 'king', 'swing', 'battery', 'bank', 'tooth', 'nurse', 'straw', 'brush', 'scale']
 >>> roles
-{'bugle': -1, 'battery': -3, 'fish': 1, 'brush': -1, 'cotton': -3, 'vet': -3, 'scale': -3, 'straw': -10, 'chest': -1, 'change': -1, 'nurse': 1, 'pants': 1, 'satellite': 1, 'dwarf': -1, 'horse': 1, 'fan': -1, 'ninja': 1, 'bank': 1, 'king': -3, 'roulette': 1, 'himalayas': -3, 'platypus': -1, 'tooth': 1, 'swing': -3, 'grass': -3}
+{'bugle': (-1, False), 'battery': (-3, False), 'fish': (1, False), 'brush': (-1, False), 'cotton': (-3, False), 'vet': (-3, False), 'scale': (-3, False), 'straw': (-10, False), 'chest': (-1, False), 'change': (-1, False), 'nurse': (1, False), 'pants': (1, False), 'satellite': (1, False), 'dwarf': (-1, False), 'horse': (1, False), 'fan': (-1, False), 'ninja': (1, False), 'bank': (1, False), 'king': (-3, False), 'roulette': (1, False), 'himalayas': (-3, False), 'platypus': (-1, False), 'tooth': (1, False), 'swing': (-3, False), 'grass': (-3, False)}
 >>> expected
 [(1, 0.4193291853533418, 'fish'), (1, 0.39795848701311254, 'pants'), (1, 0.36527604354032905, 'horse'), (1, 0.2623782121711276, 'tooth'), (1, 0.22965112665806223, 'nurse'), (1, 0.2055846539312118, 'roulette')]
 ```
+
+In the output of `roles`, the boolean value just indicates that the word has not been guessed yet.
 
 In the output for `expected`, the middle value roughly corresponds to the strength of relation. Technically, it is the cosine similarity between the two word vectors, with higher values implying more association and 0 implying independence.
 
@@ -37,15 +66,18 @@ To investigate the other options and word associations:
 >>> clues[N][3] // Same output format as expected
 ```
 
+### Starting a new game
+
 To regenerate a new word list and reassign spies (optional fixed words [w] and assignments [r]):
 
 ```
 >>> regen(w=[], r=[])
->>> words
-['lemon', 'racket', 'atlantis', 'post', 'spot', 'tie', 'pipe', 'octopus', 'straw', 'trunk', 'snow', 'mercury', 'line', 'space', 'bear', 'slip', 'saturn', 'wall', 'giant', 'shoe', 'lead', 'buffalo', 'needle', 'engine', 'chest']
->>> roles
-{'straw': -3, 'shoe': -1, 'slip': -1, 'racket': -3, 'line': 1, 'mercury': -1, 'giant': 1, 'lemon': -1, 'lead': -1, 'space': -3, 'snow': -1, 'wall': -3, 'chest': -3, 'tie': 1, 'engine': -3, 'atlantis': 1, 'needle': 1, 'spot': -3, 'bear': 1, 'trunk': -1, 'post': -10, 'saturn': 1, 'octopus': 1, 'pipe': 1, 'buffalo': -3}
 ```
+
+
+### Perf
+
+Global variables `initTime` and `findWordsTime` records the time in seconds to load the interpreter and the time to find the best words.
 
 ## Role Mapping
 
--- sim.py
+++ sim.py
@@ -1,9 +1,13 @@
 import sys
 import math
 import random
+import numpy as np
+import time
 
+beginInit = time.time()
 vectors = sys.argv[1]
-gloves = {row[0]:([float(val) for val in row[1:]]) for row in ([line.split() for line in open(vectors).read().strip().split("\n")])}
+gloves = {row[0]:(np.array([float(val) for val in row[1:]])) for row in ([line.split() for line in open(vectors).read().strip().split("\n")])}
+mags = {word: np.sqrt(np.dot(vec,vec)) for (word, vec) in gloves.items()}
 
 # random generation, should probably be hardcoded.
 codes = [x.lower() for x in open(sys.argv[2]).read().strip().split("\n")]
@@ -13,24 +17,23 @@ shuffler = [1,1,1,1,1,1,1,1,1,
             -10,
             -1, -1, -1, -1, -1, -1, -1]
 random.shuffle(shuffler)
-roles = {words[i]:shuffler[i] for i in xrange(len(words))}
+roles = {words[i]:(shuffler[i], False) for i in xrange(len(words))}
 expected = []
+endInit = time.time()
+initTime = endInit - beginInit
+findWordsTime = 0
 
 def regen(w=None, r=None):
+    # restart with new words
     global words, roles    
     if w is None:
         words = random.sample(codes, 25)
     else:
         words = w
-    if s is None:        
+    if r is None:        
         random.shuffle(shuffler)
         r = shuffler
-    roles = {words[i]:r[i] for i in xrange(len(words))}
-
-
-def mag(vec):
-    # returns magnitude, probably a math builtin exists for this
-    return math.sqrt(sum([i**2 for i in vec]))
+    roles = {words[i]:(r[i], False) for i in xrange(len(words))}
 
 def cosSim(word1, word2):
     # safely returns cosine similarity
@@ -38,8 +41,8 @@ def cosSim(word1, word2):
         return 0
     vec1 = gloves[word1]
     vec2 = gloves[word2]
-    num = sum([vec1[i]*vec2[i] for i in xrange(len(vec1))])
-    denom = mag(vec1) * mag(vec2)
+    num = np.dot(vec1, vec2)
+    denom = mags[word1]*mags[word2]
     return num/denom
 
 def cut(simScore):
@@ -52,7 +55,9 @@ def cut(simScore):
 
 def scoreWord(word1):
     # returns a list of associated words to word1 from the `good side` 
-    wordscores = [(roles[word2], cut(cosSim(word1, word2)), word2) for word2 in roles.keys()]
+    wordscores = [(role, cut(cosSim(word1, word2)), word2) 
+                  for word2, (role, revealed) in roles.items()
+                  if not revealed]
     sorted_scores = sorted(wordscores, key=lambda x: x[1])
     sorted_scores.reverse()
     for x in xrange(len(wordscores)):
@@ -65,21 +70,25 @@ def scoreWord(word1):
 
 def findWords():
     # for each number, returns the best list of words of that length
+    global findWordsTime
+    start = time.time()
     bests = {}
     bestScores = [0] * 25
     for word1 in gloves.keys():
         exp = scoreWord(word1)
         count = len(exp)
         score = sum([x[1] for x in exp])
-        if score > bestScores[count]:
+        if score > bestScores[count] and word1 not in words:
             bestScores[count] = score
             bests[count] = (word1, bestScores[count], exp)
+    end = time.time()
+    findWordsTime = end-start
     return bests
 
 def pickBest(bests):
-    global expected
     # picks the best length of list
     # bests cannot be empty
+    global expected
     bestScore = 0
     bestList = []
     for wordList in bests.items():
@@ -88,3 +97,41 @@ def pickBest(bests):
             bestList = wordList
     print (bestList[1][0], bestList[0])
     expected = bestList[1][2]
+
+def guess(word):
+    # allows user to guess in single player mode
+    global words, roles
+    if word not in roles:
+        print word + " not found in word list."
+    elif roles[word][1]:
+        print word + " was already guessed."
+    elif roles[word][0] == 1:
+        print "Correct!"
+    elif roles[word][0] == -1:
+        print "Incorrect!, An innocent was uncovered."
+    elif roles[word][0] == -3:
+        print "Incorrect!, Opposing team gets a point."
+    elif roles[word][0] == -10:
+        print "Incorrect!, GAME OVER"
+    roles[word] = (roles[word][0], True)
+
+
+def board():
+    # pretty print of board
+    display = roles.items()
+    output = "\t"
+    for x in xrange(5):
+        for y in xrange(5):
+            value = 5*x + y
+            wordinfo = display[value]
+            if not display[value][1][1]:
+                cellString = "{}".format(wordinfo[0])
+            else:
+                cellString = "{} {}".format(wordinfo[0], wordinfo[1][0])
+            output+= "{}{}".format(cellString, " " * (17-len(cellString)))
+        output += "\n\t"
+    print output
+
+def move():
+    # wrapper function for single-player mode
+    pickBest(findWords())
